e073635c804c6d8bd36d1a53320aa6715f15a1e2
Cg1989 <camille.geimer@hotmail.fr>
2019-03-18 14:33:59 +0100

    Implémentation de la classe combi et son affichage

--- Mastermind.cpp
+++ Mastermind.cpp
@@ -1,14 +1,43 @@
 #include "lib/Combinaison.h"
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void affiche_combi(Combinaison x){
-    cout << x.getn1() << "." << x.getn2() <<"." << x.getn3() <<"." << x.getn4() <<endl;
+void affiche_combi(Combinaison c){
+    cout << c.getn1() << "." << c.getn2() <<"." << c.getn3() <<"." << c.getn4() <<endl;
 }
+int myrandom (int i){
+    return rand()%i;
+}
+
+void generation_combi(Combinaison & c){
+    srand(time(NULL));
+    vector<int> vec;
+
+    for (int i=1; i<7; ++i){
+        vec.push_back(i);
+    }
+
+    random_shuffle( vec.begin(), vec.end() , myrandom );
+
+    c.setn1(vec[1]);
+    c.setn2(vec[2]);
+    c.setn3(vec[3]);
+    c.setn4(vec[4]);
+
+}
+
+
 
 int main(void){
     Combinaison cache;
-
+    generation_combi(cache);
     affiche_combi(cache);
-    }
+    
+
+    
+}
